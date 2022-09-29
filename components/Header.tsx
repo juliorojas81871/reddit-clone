ed6c496f21fc48b7092d513529c69e1864ddf0c0
Julio Rojas <juliorojas81871@gmail.com>
2022-09-28 21:18:41 -0400

    finsh header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,19 @@
 import Image from "next/image";
-import {} from "@heroicons/react/24/outline";
-import { HomeIcon, ChevronDownIcon } from "@heroicons/react/24/solid";
+import {
+  BellIcon,
+  ChatIcon,
+  GlobeIcon,
+  PlusIcon,
+  SparklesIcon,
+  SpeakerphoneIcon,
+  VideoCameraIcon,
+} from "@heroicons/react/outline";
+import {
+  HomeIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  MenuIcon,
+} from "@heroicons/react/solid";
 
 const Header = () => {
   return (
@@ -12,6 +25,35 @@ const Header = () => {
         <HomeIcon className="h-5 w-5" />
         <p className="flex-1 ml-2 hidden lg:inline">Home</p>
         <ChevronDownIcon className="h-5 w-5" />
+      </div>
+      <form className="flex flex-1 items-center space-x-2 border border-gray-200 rounded-sm bg-gray-100 px-3 py-1">
+        <SearchIcon className="w-5 h-5 text-gray-400" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          placeholder="Search Reddit"
+        />
+        <button type="submit" hidden />
+      </form>
+      <div className="flex mx-5 items-center space-x-2 text-gray-500 hidden lg:inline-flex">
+        <SparklesIcon className="icon" />
+        <GlobeIcon className="icon" />
+        <VideoCameraIcon className="icon" />
+        <hr className="h-10  border border-gray-100" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <PlusIcon className="icon" />
+        <SpeakerphoneIcon className="icon" />
+      </div>
+      <div className="ml-5 flex items-center lg:hidden">
+        <MenuIcon className="icon" />
+      </div>
+      <div className="hidden lg:flex items-center space-x-2 border border-gray-100 p- cursor-pointer">
+        <div className="relative h-5 w-5 flex-shrink-0">
+          {/* Sign in / sing out button */}
+          <Image objectFit='contain' src="/assets/icon.png" layout="fill" alt="" />
+        </div>
+        <p className="text-gray-400">Sign In</p>
       </div>
     </div>
   );
