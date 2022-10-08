32ce1a81f936681d730c0c500c798ce9a8fb222c
Julio Rojas <juliorojas81871@gmail.com>
2022-10-08 00:20:56 -0400

    fixes

--- apollo-client.js
+++ apollo-client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 // http://localhost:5001/api/eloping-marmot
 
 const client = new ApolloClient({
-  uri: `https://public024e3697a1f16892.stepzen.net/api/sweet-moth/__graphql`,
+  uri: `https://lehighton.stepzen.net/api/sweet-moth/__graphql`,
   headers: {
     Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}`,
   },
